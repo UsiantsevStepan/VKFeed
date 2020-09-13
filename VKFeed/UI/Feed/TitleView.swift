8e87c28385944f2fd8b627516bbfe50c86f2b08f
Usiantsev Stepan <usiantsevstepan@gmail.com>
2020-09-13 16:09:33 +0500

    Added "Sign out" logic

--- VKFeed/UI/Feed/TitleView.swift
+++ VKFeed/UI/Feed/TitleView.swift
@@ -40,15 +40,16 @@ class TitleView: UIView {
     func setConstraints() {
         userPictureButton.translatesAutoresizingMaskIntoConstraints = false
         userPictureButton.heightAnchor.constraint(equalToConstant: Constants.userImageSize).isActive = true
-        userPictureButton.widthAnchor.constraint(equalToConstant: Constants.userImageSize).isActive = true
-        userPictureButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        userPictureButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+        userPictureButton.widthAnchor.constraint(equalTo: userPictureButton.heightAnchor).isActive = true
+        userPictureButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 2).isActive = true
+        userPictureButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -2).isActive = true
+        userPictureButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8).isActive = true
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         label.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        label.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        label.trailingAnchor.constraint(lessThanOrEqualTo: userPictureButton.leadingAnchor, constant: 10).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        label.trailingAnchor.constraint(lessThanOrEqualTo: userPictureButton.leadingAnchor, constant: 8).isActive = true
         label.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 0).isActive = true
     }
     
@@ -56,15 +57,15 @@ class TitleView: UIView {
         label.font = UIFont.boldSystemFont(ofSize: 20)
         
         userPictureButton.contentMode = .scaleAspectFill
-        userPictureButton.layer.cornerRadius = Constants.userImageSize / 2
-        userPictureButton.layer.masksToBounds = true
         userPictureButton.clipsToBounds = true
+        userPictureButton.layer.masksToBounds = true
+        userPictureButton.layer.cornerRadius = Constants.userImageSize / 2
         userPictureButton.addTarget(self, action: #selector(logout), for: .touchUpInside)
     }
     
     func configure(with userModel: TitleViewModel) {
         label.text = userModel.userName
-    
+        
         userPictureButton.kf.setImage(with: URL(string: userModel.userPhotoUrl ?? ""), for: .normal)
     }
     
